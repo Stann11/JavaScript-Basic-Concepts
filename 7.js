ef21b30d205618f6c8c5a7cb50989bc39221df8f
Stann11 <20it122@charusat.edu.in>
2023-05-22 10:31:07 +0530

    String Operations

--- 7.js
+++ 7.js
@@ -1,1 +1,16 @@
 //String Operations 
+let firstname="       Satnam      ";
+
+console.log(firstname.length);
+
+console.log(firstname.trim().length); 
+// Trim remove the front and the end whitespace from the string
+
+console.log(firstname.toUpperCase().trim());
+ //toUpperCase turns the characters to the upper case
+
+console.log(firstname.toLowerCase().trim());
+ //toLowerCase turns the characters to the lower case
+
+console.log(firstname.trim().slice(0,3)); 
+// slice give the string from (start index, end index) last charater is not included so you will get the sting from the start to end-1 index.
