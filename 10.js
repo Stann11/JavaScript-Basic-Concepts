32047b967a4f6fc821c5be78a1e56c8e7b77f1bd
Stann11 <20it122@charusat.edu.in>
2023-05-22 11:37:34 +0530

    Template String

---
+++ 10.js
@@ -0,0 +1,9 @@
+//Template String
+
+let age=20;
+let name="Satnam";
+
+let sentence=`My name is ${name}. My age is ${age}`;            //Use back ticks(above tab) for quotes
+let sentence1="My name is "+ name +". My age is "+ age +".";    //Other method for the same sentence
+console.log(sentence);
+console.log(sentence1);
