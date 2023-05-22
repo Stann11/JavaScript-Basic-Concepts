cd40edc553eba724d383ceeb696ee750f3be028e
Stann11 <20it122@charusat.edu.in>
2023-05-22 12:00:32 +0530

    Ternery Operator

---
+++ 15.js
@@ -0,0 +1,15 @@
+//Ternery Operator
+
+let age=15;
+let drink = age>=5? "Coffee" : "Milk"; //Ternery operator  condition? "True" : "False";
+console.log(drink);
+
+//Logic with if 
+
+// if(age>=5){
+//     drink="coffee";
+// }
+// else{
+//     drink="milk";
+// }
+// console.log(drink);
