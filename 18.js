72815056f7190d8883cadb2217195d3828cf9ef8
Stann11 <20it122@charusat.edu.in>
2023-05-22 12:26:41 +0530

    If else ladder

---
+++ 18.js
@@ -0,0 +1,22 @@
+//If else ladder 
+
+let temperature = 20;
+
+if(temperature<0){
+    console.log("Extremely cold");
+}
+else if(temperature<16){
+    console.log("Cold Outside");
+}
+else if(temperature<25){
+    console.log("Weather is OK");
+}
+else if(temperature<35){
+    console.log("Let's go for swim");
+}
+else if(temperature<45){
+    console.log("Turn on AC");
+}
+else{
+    console.log("Too Hot!");
+}
