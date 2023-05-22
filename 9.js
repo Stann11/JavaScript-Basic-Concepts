9dcf4341ab67e8315e4f3fc78b0c0f0ed482ce82
Stann11 <20it122@charusat.edu.in>
2023-05-22 11:37:16 +0530

    String concatination

---
+++ 9.js
@@ -0,0 +1,11 @@
+// String Concatination
+
+let name="Satnam";
+let lastname="Sandhu";
+let fullname= name+" "+lastname;
+console.log(fullname); 
+
+let string1="29";
+let string2="30";
+console.log(string1+string2);
+console.log((+string1)+(+string2));
