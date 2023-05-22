bdb4bdbaae247a690dac24bf7cc754aad52e22af
Stann11 <20it122@charusat.edu.in>
2023-05-22 15:18:03 +0530

    Arrays

---
+++ 26.js
@@ -0,0 +1,20 @@
+//Arrays
+
+let fruits = ["apple","banana","mango"];
+let number1=[1,2,3,4];
+let mix=[1,"apple", 3.14];
+
+console.log(number1);
+
+console.log(mix);
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+fruits[1]="Jamun";
+console.log(fruits);
+
+// Array is an object in Java Script
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));
