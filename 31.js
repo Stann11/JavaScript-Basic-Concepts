4a8d517e2227fd7230db57de56b6908d78986aaf
Stann11 <20it122@charusat.edu.in>
2023-05-22 15:54:29 +0530

    Using Constant for creating array

---
+++ 31.js
@@ -0,0 +1,8 @@
+//Use constant for creating array
+
+const arr1=["Apple", "Banana", "Mango"];
+
+//arr1=["Grapes","Jamun"]; //error
+
+arr1.push("Chicoo"); //array manipulation methods can be used on the constant
+console.log(arr1);
