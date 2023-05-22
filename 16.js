525bea9a8a37e26a8ff0fc88b4fb62881f36ce54
Stann11 <20it122@charusat.edu.in>
2023-05-22 12:00:59 +0530

    AND and OR operators

---
+++ 16.js
@@ -0,0 +1,18 @@
+//and || or operators 
+
+let name="Satnam";
+let age=20;
+
+// and 
+if(name[0]==="S" && age>=18){
+    console.log("Your name starts with S and you are adult")
+}else{
+    console.log("Your name does'nt start with S or you are not an adult")
+}
+
+//or 
+if(name[0]==="S" || age>=18){
+    console.log("Inside if");
+}else{
+    console.log("Inside else");
+}
