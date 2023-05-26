23c12bf66b7f4bb8b547803b0c1d710c95d5fc02
Stann11 <20it122@charusat.edu.in>
2023-05-26 11:16:43 +0530

    Object inside array

---
+++ 42.js
@@ -0,0 +1,11 @@
+//Object inside array
+const users=[
+    {user:1,name:"Satnam",gender:"Male"},
+    {user:2,name:"Vivek",gender:"Male"},
+    {user:3,name:"Yash",gender:"Male"}
+]
+for(let user of users){
+    console.log(user);
+}
+
+//You can access the userid, name, gender by using the dot notation 
