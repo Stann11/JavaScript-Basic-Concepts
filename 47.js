22a9a140ab0d894992d0178c966753bb2e332cec
Stann11 <20it122@charusat.edu.in>
2023-05-26 13:25:49 +0530

    Hoisting

---
+++ 47.js
@@ -0,0 +1,9 @@
+// Hoisting 
+
+//It works only for normal function and does not work with function expression and arrow function
+
+hello();
+
+function hello(){
+    console.log("Hello World!");
+}
