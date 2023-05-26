efb93544b16e2a8cde9c406772094eb6174dabdd
Stann11 <20it122@charusat.edu.in>
2023-05-26 13:25:41 +0530

    Arrow Function

---
+++ 46.js
@@ -0,0 +1,11 @@
+//Arrow function
+
+const Happybirthday=() =>{
+    console.log("Happy birth day to you........")
+}
+Happybirthday();
+
+const Add = (num1,num2) =>{
+    return num1+num2;
+}
+console.log(Add(1,2));
