9c004bfacff4b91826d0e27976ff0611ce32481d
Stann11 <20it122@charusat.edu.in>
2023-05-26 11:16:28 +0530

    Object Destructuring

---
+++ 41.js
@@ -0,0 +1,10 @@
+// Object destructuring (Same as array)
+
+const band={
+    bandname:"Band1",
+    famoussong:"Song1"
+};
+
+const{bandname,famoussong}=band;
+console.log(bandname);
+console.log(famoussong);
