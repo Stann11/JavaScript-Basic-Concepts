e0ede22a4188a22880ae097395ec9b36efe1cbc2
Stann11 <20it122@charusat.edu.in>
2023-05-26 13:25:20 +0530

    Function Expression

---
+++ 45.js
@@ -0,0 +1,16 @@
+//Function expression
+
+const happy= function(){
+    console.log("Happy birthday to you .......");
+}
+happy();
+
+const twoplusfour=function(){
+    console.log(2+4);
+}
+twoplusfour();
+
+const value=function(num1,num2){
+    return num1+num2;
+}
+console.log(value(1,2));
