ba9124b0bb8430ef23b9a1396ef5923de3ab3f60
Stann11 <20it122@charusat.edu.in>
2023-05-26 11:15:04 +0530

    Objects

---
+++ 36.js
@@ -0,0 +1,12 @@
+// objects 
+
+const person={name:"Satnam",age:20, hobbies:["music","cricket"]};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+console.log(person.hobbies[0]);
+console.log(person.hobbies[1]);
+
+person.gender="male";
+console.log(person);
